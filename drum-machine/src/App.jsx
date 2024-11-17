8673e5d85edd4a8e1c5b0f2508ef0712d4fb3d17
paulshriner <shrinerpaul@gmail.com>
2024-11-17 16:00:40 -0500

    drum-machine: add styles

--- drum-machine/src/App.jsx
+++ drum-machine/src/App.jsx
@@ -65,43 +65,46 @@ function App() {
 
   return (
     <div id="drum-machine">
-      <div id="display"></div>
-      <button className="drum-pad" id="heater-1" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" className="clip" id="Q"></audio>
-        Q
-      </button>
-      <button className="drum-pad" id="heater-2" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3" className="clip" id="W"></audio>
-        W        
-      </button>
-      <button className="drum-pad" id="heater-3" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3" className="clip" id="E"></audio>
-        E        
-      </button>
-      <button className="drum-pad" id="heater-4" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3" className="clip" id="A"></audio>
-        A        
-      </button>
-      <button className="drum-pad" id="clap" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3" className="clip" id="S"></audio>
-        S      
-      </button>
-      <button className="drum-pad" id="open-hh" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3" className="clip" id="D"></audio>
-        D        
-      </button>
-      <button className="drum-pad" id="kick-n-hat" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3" className="clip" id="Z"></audio>
-        Z        
-      </button>
-      <button className="drum-pad" id="kick" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3" className="clip" id="X"></audio>
-        X        
-      </button>
-      <button className="drum-pad" id="closed-hh" onClick={handleButtonPress}>
-        <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3" className="clip" id="C"></audio>
-        C        
-      </button>
+      <div id="drum-pads">
+        <button className="drum-pad" id="heater-1" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3" className="clip" id="Q"></audio>
+          Q
+        </button>
+        <button className="drum-pad" id="heater-2" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3" className="clip" id="W"></audio>
+          W        
+        </button>
+        <button className="drum-pad" id="heater-3" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3" className="clip" id="E"></audio>
+          E        
+        </button>
+        <button className="drum-pad" id="heater-4" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3" className="clip" id="A"></audio>
+          A        
+        </button>
+        <button className="drum-pad" id="clap" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3" className="clip" id="S"></audio>
+          S      
+        </button>
+        <button className="drum-pad" id="open-hh" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3" className="clip" id="D"></audio>
+          D        
+        </button>
+        <button className="drum-pad" id="kick-n-hat" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3" className="clip" id="Z"></audio>
+          Z        
+        </button>
+        <button className="drum-pad" id="kick" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3" className="clip" id="X"></audio>
+          X        
+        </button>
+        <button className="drum-pad" id="closed-hh" onClick={handleButtonPress}>
+          <audio src="https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3" className="clip" id="C"></audio>
+          C        
+        </button>
+      </div>
+      {/* Thanks https://www.editpad.org/tool/invisible-character for invisible char */}
+      <p id="display">‎ </p>
     </div>
   )
 }
