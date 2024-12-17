3389d2f9a9802be7debeacc6564cdf0b300f554f
paulshriner <shrinerpaul@gmail.com>
2024-12-17 18:36:36 -0500

    255-clock: passing tests

--- 255-clock/src/App.jsx
+++ 255-clock/src/App.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [breakLength, setBreakLength] = useState(5);
-  const [sessionLength, setSessionLength] = useState(25);
+  const INIT_BREAK_LENGTH = 5;
+  const INIT_SESSION_LENGTH = 25;
+  const [breakLength, setBreakLength] = useState(INIT_BREAK_LENGTH);
+  const [sessionLength, setSessionLength] = useState(INIT_SESSION_LENGTH);
   const [status, setStatus] = useState("session");
   const [statusColor, setStatusColor] = useState("black");
   const [clockActive, setClockActive] = useState(false);
-  const [timeRemaining, setTimeRemaining] = useState(sessionLength * 60000);
+  const [timeRemaining, setTimeRemaining] = useState(INIT_SESSION_LENGTH * 60000);
 
   // Thanks https://upmostly.com/tutorials/build-a-react-timer-component-using-hooks for help with timer
   useEffect(() => {
@@ -22,9 +24,12 @@ function App() {
       // Update color when less than 1 minute remains
       timeRemaining === 59000 && setStatusColor("red");
 
+      // Play sound at 0:00
+      timeRemaining === 0 && document.getElementById("beep").play();
+
       // Update time remaining based on Break or Session
-      if (timeRemaining < 0) {
-        document.getElementById("beep").play();
+      // wait until -1000 so that 0:00 displays for a second
+      if (timeRemaining < -1000) {
         if (status === "session") {
           setStatus("break");
           setTimeRemaining(breakLength * 60000);
@@ -87,14 +92,14 @@ function App() {
 
     // Start/Pause the clock
     if (e.target.id === "start_stop") {
-      clockActive ? setClockActive(false) : setClockActive(true);
+      setClockActive(!clockActive);
     }
   }
 
   // Thanks https://stackoverflow.com/questions/61791234/how-to-display-countdown-timer-in-react for time formatting
-  // Pads a zero if the second is one digit (less than 10)
+  // Pads a zero if the digit is one (less than 10)
   const padTime = time => {
-    return String(time).length === 1 ? `0${time}` : `${time}`;
+    return time < 10 ? `0${time}` : `${time}`;
   }
 
   const format = time => {
@@ -105,16 +110,16 @@ function App() {
     const seconds = time % 60;
 
     // Return formatted sring using padTime for seconds
-    return `${minutes}:${padTime(seconds)}`;
+    return timeRemaining > 0 ? `${padTime(minutes)}:${padTime(seconds)}` : "00:00";
   }
 
   const reset = () => {
-    setBreakLength(5);
-    setSessionLength(25);
+    setBreakLength(INIT_BREAK_LENGTH);
+    setSessionLength(INIT_SESSION_LENGTH);
     setStatus("session");
     setStatusColor("black");
     setClockActive(false);
-    setTimeRemaining(sessionLength * 60000);
+    setTimeRemaining(INIT_SESSION_LENGTH * 60000);
     // Thanks https://stackoverflow.com/questions/14834520/html5-audio-stop-function for stopping audio
     document.getElementById("beep").pause();
     document.getElementById("beep").currentTime = 0;
@@ -124,13 +129,13 @@ function App() {
     <div id="main-container">
       <h1>25 + 5 Clock</h1>
       <h2 id="break-label">Break Length</h2>
-      <div class="length-controls">
+      <div className="length-controls">
         <button id="break-decrement" onClick={handleClick}>V</button>
         <div id="break-length">{breakLength}</div>
         <button id="break-increment" onClick={handleClick}>^</button>
       </div>
       <h2 id="session-label">Session Length</h2>
-      <div class="length-controls">
+      <div className="length-controls">
         <button id="session-decrement" onClick={handleClick}>V</button>
         <div id="session-length">{sessionLength}</div>
         <button id="session-increment" onClick={handleClick}>^</button>
@@ -140,7 +145,7 @@ function App() {
       <button id="start_stop" onClick={handleClick}>Start/Stop</button>
       <button id="reset" onClick={reset}>Reset</button>
       {/* Thanks https://mixkit.co/free-sound-effects/beep/ for sound effect */}
-      <audio id="beep" src="https://assets.mixkit.co/active_storage/sfx/221/221.wav"></audio>
+      <audio id="beep" src="https://assets.mixkit.co/active_storage/sfx/1084/1084.wav"></audio>
     </div>
   )
 }
